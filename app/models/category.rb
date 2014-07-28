8674e4b1dd12cc0ec3209edeaae17a92792f8b4c
mahsa <m_mw7462@yahoo.com>
2014-07-28 16:40:44 +0430

    set some design to category pages

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,20 @@
 # encoding: UTF-8
 class Category < ActiveRecord::Base
   has_many :products
+  
+  def title    
+    if I18n.locale == :en
+      self.read_attribute("title_en")
+    else
+      self.read_attribute("title_fa")
+    end    
+  end
+  
+  def description    
+    if I18n.locale == :en
+      self.read_attribute("description_en")
+    else
+      self.read_attribute("description_fa")
+    end    
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,20 @@
 class Product < ActiveRecord::Base
   has_many :pictures
   belongs_to :category
+  
+  def title    
+    if I18n.locale == :en
+      self.read_attribute("title")
+    else
+      self.read_attribute("title_fa")
+    end    
+  end
+  
+  def description    
+    if I18n.locale == :en
+      self.read_attribute("description")
+    else
+      self.read_attribute("description_fa")
+    end    
+  end
 end
