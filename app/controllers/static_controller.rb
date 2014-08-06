ec3b35dca582af7fbf44b61b7763c7a0ae5fa70c
mahsa <m_mw7462@yahoo.com>
2014-08-06 13:49:42 +0430

    set dynamic info of about and contact pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
   def home
     @categories = Category.all
+    @about = Page.where(title: "About us")
+    @contact = Page.where(title: "Contact us")
+    
   end
 end
