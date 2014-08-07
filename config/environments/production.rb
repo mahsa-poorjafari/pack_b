e1b6726667534af8d574fced942bdf4b1468c6e4
mahsa <m_mw7462@yahoo.com>
2014-08-07 11:25:35 +0430

    fix error of .configure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+PackB::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
