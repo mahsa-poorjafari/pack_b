be5e22daa4b8d6835537fa12f21d695fe6db12ea
mahsa <m_mw7462@yahoo.com>
2014-08-07 12:44:03 +0430

    add statice asstes loade on server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,10 @@ PackB::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.compile = false
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
