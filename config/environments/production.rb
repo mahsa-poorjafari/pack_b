c84a2b97729235b2cfdaadb222397aade93dc868
mahsa <m_mw7462@yahoo.com>
2014-08-07 10:34:39 +0430

    change some design and database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
-PackB::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
+  # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
@@ -27,7 +27,11 @@ PackB::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  
+  config.serve_static_assets = true
+  config.assets.compress = true
+  config.assets.compile = false
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -66,7 +70,7 @@ PackB::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found).
+  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
@@ -77,4 +81,7 @@ PackB::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 end
