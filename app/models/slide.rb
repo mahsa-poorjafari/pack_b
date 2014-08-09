075ff4fe6db8dc67ea68fe013b95559744cb8312
mahsa <m_mw7462@yahoo.com>
2014-08-09 11:34:38 +0430

    change name of image for slider as static

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -13,10 +13,12 @@ class Slide < ActiveRecord::Base
   def self.get_supersized_js
     ret = []
     
-    [1,2,3].each do |slide|
-      ret << "{image : \"assets/slide#{slide}.jpg\", title : \"<div class='slide-content'>description#{slide}</div>\", thumb : '', url : ''}"
-    end
-    ret.join(",")
+    #[1,2,3].each do |slide|
+    ret << "{image : \"assets/slide1-eb5e74ff4a5d6b7240247b826751aaea.jpg\", title : \"<div class='slide-content'>description</div>\", thumb : '', url : ''} , 
+            {image : \"assets/slide2-2a522a37d3e9da964ecd8b22c219d99a.jpg\", title : \"<div class='slide-content'>description</div>\", thumb : '', url : ''} ,
+            {image : \"assets/slide3-43d724a09f019f63aa871e17942c2612.jpg\", title : \"<div class='slide-content'>description</div>\", thumb : '', url : ''}"
+    #end
+    #ret.join(",")
   end
 
 end
