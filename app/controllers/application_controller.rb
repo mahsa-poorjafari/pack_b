4261f50009b9c641f10198955a94f6687dad67aa
mahsa <m_mw7462@yahoo.com>
2014-08-30 15:38:18 +0430

    remove befor_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_locale
   include ApplicationHelper
-  before_filter :authenticate
+  
   
   protected
     def check_autentication
