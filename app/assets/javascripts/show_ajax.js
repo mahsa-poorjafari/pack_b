c35c1789420cb10888ff3a58cc20fcd1ba3ae1d5
mahsa <m_mw7462@yahoo.com>
2014-09-01 12:39:30 +0430

    add nanoscroll for details product

--- app/assets/javascripts/show_ajax.js
+++ app/assets/javascripts/show_ajax.js
@@ -6,8 +6,8 @@ $('#show-details img.close').click(function() {
   $('#show-details').fadeOut(1000);
   $('.project-pic').fadeIn(500);
 });
+
 $('#show-details').click(function(event){
-  event.stopPropagation();
-  
+  event.stopPropagation();  
 });
 
